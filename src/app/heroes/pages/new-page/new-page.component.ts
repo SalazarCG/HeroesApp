015db56e0b32d6460fdc8923b66f88022aed9892
Cristian <389336@alu.murciaeduca.es>
2025-02-02 01:43:01 +0100

    construimos el formulario reactivo e introducimos la forma de actualizar con snackbar

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { Router } from '@angular/router';
-import { Publisher } from '../../interfaces/hero.interface';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { switchMap } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
   styles: ``,
 })
-export class NewPageComponent {
-  
+export class NewPageComponent implements OnInit {
+
   public heroForm = new FormGroup({
     id: new FormControl<string>(''),
     superhero: new FormControl<string>('', { nonNullable: true }),
@@ -20,7 +23,26 @@ export class NewPageComponent {
     alt_img: new FormControl(''),
   });
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private snackbar: MatSnackBar,
+  ) {}
+
+
+  ngOnInit(): void {
+    if ( !this.router.url.includes('edit'))return;
+
+    this.activatedRoute.params
+    .pipe(
+      switchMap( ({id}) => this.heroesService.getHeroById(id) ),
+    ).subscribe( hero => {
+        if (!hero) return this.router.navigateByUrl('/');
+          this.heroForm.reset(hero);
+          return;
+      });
+  }
 
   public publishers = [
     { id: 'DC Comics', desc: 'DC - Comics' },
@@ -30,4 +52,38 @@ export class NewPageComponent {
   goBack(): void {
     this.router.navigateByUrl('/heroes/list');
   }
+
+  get currentHero(): Hero {
+    const hero = this.heroForm.value as Hero;
+    return hero;
+  }
+
+  onSubmit(): void {
+
+    if (this.heroForm.invalid) return;
+
+    if ( this.currentHero.id){
+      this.heroesService.updateHero(this.currentHero)
+        .subscribe( hero => {
+          this.showSnackbar(`${hero.superhero} actualizado`);
+
+        });
+        return;
+    }
+
+    this.heroesService.addHero(this.currentHero)
+      .subscribe( hero => {
+        //TODO mostrar snackbar
+        this.router.navigate(['/heroes/edit', hero.id]);
+        this.showSnackbar(`${hero.superhero} creado`);
+      });
+  }
+
+  showSnackbar(message: string): void {
+    this.snackbar.open(message, 'ok!', {
+      duration: 2500,
+    });
+  }
+
+
 }
